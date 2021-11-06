00272ad27ff94a1340539a3eec1551690f8c3dc5
Mihail Marian <mihailmarian12@gmail.com>
2021-11-06 22:03:13 +0000

    better response codes

--- main/views.py
+++ main/views.py
@@ -12,36 +12,37 @@ class CalculateView(views.APIView):
         url = request.data.get("url")
         results = sentiment_analyze(url)
 
-        context = {
-            "success": results["success"],
-            "url": url,
-        }
+        context = {"url": url}
 
-        if results["success"]:
+        if not (results["success"]):
+            message = results["message"]
 
-            website = WebsiteModel(
-                url=results["url_analyzed"],
-                score=results["score"],
-                raw_data=results["raw_data"],
-            )
+            website = FailedWebsiteModel(url=url, error_message=message)
             website.save()
 
-            website_stand_li = sen_stand(
-                website_model_list=WebsiteModel.objects.all(),
-                range_min=0,
-                range_max=100,
-            )
+            context["error"] = message
 
-            context.update(
-                [
-                    ("website_raw_data", results["raw_data"]),
-                    ("website_li", website_stand_li),
-                    ("url_analyzed", results["url_analyzed"]),
-                ]
-            )
-        else:
-            website = FailedWebsiteModel(url=url, error_message=results["message"])
-            context["message"] = results["message"]
-            website.save()
+            return Response(status=400, data=context)
+
+        website = WebsiteModel(
+            url=results["url_analyzed"],
+            score=results["score"],
+            raw_data=results["raw_data"],
+        )
+        website.save()
+
+        website_stand_li = sen_stand(
+            website_model_list=WebsiteModel.objects.all(),
+            range_min=0,
+            range_max=100,
+        )
+
+        context.update(
+            [
+                ("website_raw_data", results["raw_data"]),
+                ("website_li", website_stand_li),
+                ("url_analyzed", results["url_analyzed"]),
+            ]
+        )
 
         return Response(context)
