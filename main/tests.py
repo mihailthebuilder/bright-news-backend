1f17d2045ccfdb5914e1358c076043177b219548
Mihail Marian <mihailmarian12@gmail.com>
2021-11-06 21:12:03 +0000

    save work

--- main/tests.py
+++ main/tests.py
@@ -1,7 +1,17 @@
 from django.test import TestCase
 
 # Complete test for the main app.
+
+API_PATH = "/api/calculate"
+
+
 class TestMain(TestCase):
-    def test_main_page(self):
-        response = self.client.post(path="/api/calculate", data={"url": "ft.com"})
+    def good_request(self):
+        response = self.client.post(path=API_PATH, data={"url": "ft.com"})
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(type(response.data["score"]), int)
+
+    def bad_request(self):
+        response = self.client.post(path=API_PATH, data={"url": "ssre"})
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data["error"], "Bad request")
