23d3312e5ca52e3aeb3028dea7784c0469970438
Mihail Marian <mihailmarian12@gmail.com>
2021-11-06 21:28:49 +0000

    better assertion built

--- main/tests.py
+++ main/tests.py
@@ -9,9 +9,13 @@ class TestMain(TestCase):
     def test_good_request(self):
         response = self.client.post(path=API_PATH, data={"url": "ft.com"})
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(type(response.data["score"]), int)
 
-    def test_bad_request(self):
-        response = self.client.post(path=API_PATH, data={"url": "ssre"})
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.data["error"], "Bad request")
+        first_scored_url = response.data["website_li"][0]
+
+        self.assertEqual(type(first_scored_url["score"]), float)
+        self.assertEqual(type(first_scored_url["url"]), str)
+
+    # def test_bad_request(self):
+    #     response = self.client.post(path=API_PATH, data={"url": "ssre"})
+    #     self.assertEqual(response.status_code, 400)
+    #     self.assertEqual(response.data["error"], "Bad request")
