d327467b8f921fd0c6cb3d040d4d55e1fe73bfdf
Mihail Marian <mihailmarian12@gmail.com>
2021-11-06 21:14:17 +0000

    rewording

--- main/tests.py
+++ main/tests.py
@@ -6,12 +6,12 @@ API_PATH = "/api/calculate"
 
 
 class TestMain(TestCase):
-    def good_request(self):
+    def test_good_request(self):
         response = self.client.post(path=API_PATH, data={"url": "ft.com"})
         self.assertEqual(response.status_code, 200)
         self.assertEqual(type(response.data["score"]), int)
 
-    def bad_request(self):
+    def test_bad_request(self):
         response = self.client.post(path=API_PATH, data={"url": "ssre"})
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.data["error"], "Bad request")
